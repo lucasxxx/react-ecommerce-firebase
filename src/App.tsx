aeae2705a06b0ba5331bab7db97cfe85a172d987
lucasxxx <lucas.santanadesouza@gmail.com>
2021-05-11 23:23:48 -0300

    header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Header from './components/Header'
+import './styles.scss';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
     </div>
   );
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './header.scss'
+
+const Header = () => {
+   return (
+     <header className="header">
+       <div className="wrapper">
+         <div className="logo">
+           <h1>SAMPLE</h1>
+         </div>
+       </div>
+     </header> 
+   );
+};
+
+export default Header;
